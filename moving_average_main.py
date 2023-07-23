8a3e1a9c7207f3d104325b97c406a10f1db8697b
theo-xiao-sg <theo.xiao.sg@gmail.com>
2023-07-23 17:01:38 +0800

    no more company name from yfinance

--- moving_average_main.py
+++ moving_average_main.py
@@ -10,6 +10,9 @@ from moving_average_strategy import moving_average_strategy
 start = '2010-01-01'
 end = datetime.today().strftime('%Y-%m-%d')
 
+company_name_ticker = {'^NDX': 'Nasdaq 100', 'TSLA': 'Tesla', 'MSFT': 'Microsoft', 'AAPL': 'Apple', 'AMZN': 'Amazon',
+                       'goog': 'Google', '0700.HK': 'Tencent', 'NFLX': 'Netflix'}
+
 # Define the ticker symbol for the stocks
 # ticker = '^NDX'
 # ticker = 'TSLA'
@@ -20,11 +23,8 @@ ticker = 'MSFT'
 # ticker = '0700.HK'
 # ticker = 'NFLX'
 
-# Create a Ticker object for the desired ticker symbol
-ticker_yf = yf.Ticker(ticker)
-
 # Get the short name of the ticker
-company_name = ticker_yf.info["shortName"]
+company_name = company_name_ticker[ticker]
 
 # Download historical data for the stocks from Yahoo Finance
 download_data = yf.download(ticker, start=start, end=end)
